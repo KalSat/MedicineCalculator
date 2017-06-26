88b9c7314e0d8bb44cb5255efd446a66ad0104b2
程彪 <chengbiao@meizu.com>
2017-06-26 20:45:56 +0800

    添加两个底栏按钮

--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -1,12 +1,16 @@
 <template>
     <div id="page-root">
-        <mu-appbar id="app-bar" title="Douban Movie">
+        <mu-appbar id="app-bar" title="Medicine Calculator">
             <mu-icon-button icon='menu' slot="left"/>
             <mu-icon-button icon="search" slot="right"/>
         </mu-appbar>
         <div class="content">
             <img id="img-logo" src="../assets/logo.png">
             <hello></hello>
+        </div>
+        <div id="bottom-bar">
+            <mu-float-button icon="add" class="float-button"/>
+            <mu-float-button icon="done" class="float-button"/>
         </div>
     </div>
 </template>
@@ -17,12 +21,34 @@
     }
 
     .content {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        padding-top: 56px;
+
+        background-color: #66ccff;
+
+        max-width: 74em;
+        padding-top: 3.5em;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    #bottom-bar {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        margin: auto;
+        max-width: 64em;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
+    .float-button {
+        bottom: 1.5em;
     }
 </style>
 
